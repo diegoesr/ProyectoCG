9c0f78176a0c8af44b9bd9009c0cee8d8f2b016e
HectorGenaroRKR <72625421+HectorGenaroRKR@users.noreply.github.com>
2025-05-05 07:27:16 -0600

    Add files via upload

---
+++ ProyectoLab.cpp
@@ -0,0 +1,621 @@
+//Borrador de proyecto
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader_light.h"
+#include "Camera.h"
+#include "Texture.h"
+#include "Sphere.h"
+#include"Model.h"
+#include "Skybox.h"
+
+//para iluminación
+#include "CommonValues.h"
+#include "DirectionalLight.h"
+#include "PointLight.h"
+#include "SpotLight.h"
+#include "Material.h"
+const float toRadians = 3.14159265f / 180.0f;
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+
+//Texturas
+Texture pisoTexture;
+
+//Mundo Finn
+Model Finn;
+Model PIzq;
+Model PDer;
+Model BIzq;
+Model BDer;
+Model Mochila;
+Model Enchiridion;
+Model Topo;
+Model Banca;
+Model Pizza;
+Model Picnic;
+Model Tiro_blanco;
+Model Arco_finn;
+Model Letrero_finn;
+Model Prismo;
+
+
+
+//Mundo Invencible
+Model Regalos;
+Model Bateo;
+Model Nave;
+Model Baloncesto;
+Model Casa_arbol;
+
+Skybox skybox;
+
+//materiales
+Material Material_brillante;
+Material Material_opaco;
+
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+// luz direccional
+DirectionalLight mainLight;
+//para declarar varias luces de tipo pointlight
+PointLight pointLights[MAX_POINT_LIGHTS];
+SpotLight spotLights[MAX_SPOT_LIGHTS];
+
+// Vertex Shader
+static const char* vShader = "shaders/shader_light.vert";
+
+// Fragment Shader
+static const char* fShader = "shaders/shader_light.frag";
+
+
+//función de calculo de normales por promedio de vértices 
+void calcAverageNormals(unsigned int* indices, unsigned int indiceCount, GLfloat* vertices, unsigned int verticeCount,
+	unsigned int vLength, unsigned int normalOffset)
+{
+	for (size_t i = 0; i < indiceCount; i += 3)
+	{
+		unsigned int in0 = indices[i] * vLength;
+		unsigned int in1 = indices[i + 1] * vLength;
+		unsigned int in2 = indices[i + 2] * vLength;
+		glm::vec3 v1(vertices[in1] - vertices[in0], vertices[in1 + 1] - vertices[in0 + 1], vertices[in1 + 2] - vertices[in0 + 2]);
+		glm::vec3 v2(vertices[in2] - vertices[in0], vertices[in2 + 1] - vertices[in0 + 1], vertices[in2 + 2] - vertices[in0 + 2]);
+		glm::vec3 normal = glm::cross(v1, v2);
+		normal = glm::normalize(normal);
+
+		in0 += normalOffset; in1 += normalOffset; in2 += normalOffset;
+		vertices[in0] += normal.x; vertices[in0 + 1] += normal.y; vertices[in0 + 2] += normal.z;
+		vertices[in1] += normal.x; vertices[in1 + 1] += normal.y; vertices[in1 + 2] += normal.z;
+		vertices[in2] += normal.x; vertices[in2 + 1] += normal.y; vertices[in2 + 2] += normal.z;
+	}
+
+	for (size_t i = 0; i < verticeCount / vLength; i++)
+	{
+		unsigned int nOffset = i * vLength + normalOffset;
+		glm::vec3 vec(vertices[nOffset], vertices[nOffset + 1], vertices[nOffset + 2]);
+		vec = glm::normalize(vec);
+		vertices[nOffset] = vec.x; vertices[nOffset + 1] = vec.y; vertices[nOffset + 2] = vec.z;
+	}
+}
+
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		//	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+	unsigned int vegetacionIndices[] = {
+	   0, 1, 2,
+	   0, 2, 3,
+	   4,5,6,
+	   4,6,7
+	};
+
+	GLfloat vegetacionVertices[] = {
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.0f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+
+		0.0f, -0.5f, -0.5f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, -0.5f, 0.5f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.5f,		1.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, -0.5f,		0.0f, 1.0f,		0.0f, 0.0f, 0.0f,
+
+
+	};
+	
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh *obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+	Mesh* obj4 = new Mesh();
+	obj4->CreateMesh(vegetacionVertices, vegetacionIndices, 64, 12);
+	meshList.push_back(obj4);
+
+	calcAverageNormals(indices, 12, vertices, 32, 8, 5);
+
+	calcAverageNormals(vegetacionIndices, 12, vegetacionVertices, 64, 8, 5);
+
+}
+
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 0.5f);
+
+	
+	//---------Texturas------------
+	pisoTexture = Texture("Textures/pavimento.tga");
+	pisoTexture.LoadTextureA();
+	
+	
+	
+	//---------Mundo Finn----------
+	//Finn
+	Finn = Model();
+	Finn.LoadModel("Models/finn/Cuerpo_finn.obj");
+	PIzq = Model();
+	PIzq.LoadModel("Models/finn/PIzq.obj");
+	PDer = Model();
+	PDer.LoadModel("Models/finn/PDer.obj");
+	BIzq = Model();
+	BIzq.LoadModel("Models/finn/BIzq.obj");
+	BDer = Model();
+	BDer.LoadModel("Models/finn/BDer.obj");
+	Mochila = Model();
+	Mochila.LoadModel("Models/finn/mochila.obj");
+	Enchiridion = Model();
+	Enchiridion.LoadModel("Models/finn/enchiridion.obj");
+	Topo = Model();
+	Topo.LoadModel("Models/finn/topo.obj");
+	Banca = Model();
+	Banca.LoadModel("Models/finn/Banca.obj");
+	Pizza = Model();
+	Pizza.LoadModel("Models/finn/pizza.obj");
+	Picnic = Model();
+	Picnic.LoadModel("Models/finn/picnic.obj");
+	Tiro_blanco = Model();
+	Tiro_blanco.LoadModel("Models/finn/tiro_al_blanco.obj");
+	Arco_finn = Model();
+	Arco_finn.LoadModel("Models/finn/arco_opt.obj");
+	Letrero_finn = Model();
+	Letrero_finn.LoadModel("Models/finn/Letrero.obj");
+	Prismo = Model();
+	Prismo.LoadModel("Models/finn/prismo.obj");
+
+
+
+
+	//---------Mundo Invencible----------
+	//Mark
+	Regalos = Model();
+	Regalos.LoadModel("Models/Invencible/regalos.obj");
+	Bateo = Model();
+	Bateo.LoadModel("Models/Invencible/bateo.obj");
+	Nave = Model();
+	Nave.LoadModel("Models/Invencible/nave.obj");
+	Baloncesto = Model();
+	Baloncesto.LoadModel("Models/Invencible/baloncesto.obj");
+	Casa_arbol = Model();
+	Casa_arbol.LoadModel("Models/Invencible/casa_eve.obj");
+
+	//Skybox
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/bosque/posx.jpg"); //right
+	skyboxFaces.push_back("Textures/bosque/negx.jpg"); //left
+	skyboxFaces.push_back("Textures/bosque/negy.jpg"); //bottom
+	skyboxFaces.push_back("Textures/bosque/posy.jpg"); //top
+	skyboxFaces.push_back("Textures/bosque/posz.jpg"); //back
+	skyboxFaces.push_back("Textures/bosque/negz.jpg"); //front
+
+	skybox = Skybox(skyboxFaces);
+
+	Material_brillante = Material(4.0f, 256);
+	Material_opaco = Material(0.3f, 4);
+
+
+	//luz direccional, sólo 1 y siempre debe de existir
+	mainLight = DirectionalLight(1.0f, 1.0f, 1.0f,
+		0.3f, 0.3f,
+		0.0f, 0.0f, -1.0f);
+	//contador de luces puntuales
+	unsigned int pointLightCount = 0;
+	//Declaración de primer luz puntual
+	pointLights[0] = PointLight(1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f,
+		-6.0f, 1.5f, 1.5f,
+		0.3f, 0.2f, 0.1f);
+	pointLightCount++;
+
+	unsigned int spotLightCount = 0;
+	//linterna
+	spotLights[0] = SpotLight(1.0f, 1.0f, 1.0f,
+		0.0f, 2.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, -1.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		5.0f);
+	spotLightCount++;
+
+	//luz fija
+	spotLights[1] = SpotLight(0.0f, 1.0f, 0.0f,
+		1.0f, 2.0f,
+		5.0f, 10.0f, 0.0f,
+		0.0f, -5.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		15.0f);
+	spotLightCount++;
+	
+	//se crean mas luces puntuales y spotlight 
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+	////Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		// Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformEyePosition = shaderList[0].GetEyePositionLocation();
+		uniformColor = shaderList[0].getColorLocation();
+		
+		//información en el shader de intensidad especular y brillo
+		uniformSpecularIntensity = shaderList[0].GetSpecularIntensityLocation();
+		uniformShininess = shaderList[0].GetShininessLocation();
+
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		glUniform3f(uniformEyePosition, camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+
+		// luz ligada a la cámara de tipo flash
+		//sirve para que en tiempo de ejecución (dentro del while) se cambien propiedades de la luz
+			glm::vec3 lowerLight = camera.getCameraPosition();
+		lowerLight.y -= 0.3f;
+		spotLights[0].SetFlash(lowerLight, camera.getCameraDirection());
+
+		//información al shader de fuentes de iluminación
+		shaderList[0].SetDirectionalLight(&mainLight);
+		shaderList[0].SetPointLights(pointLights, pointLightCount);
+		shaderList[0].SetSpotLights(spotLights, spotLightCount);
+
+
+		//Piso
+		glm::mat4 model(1.0);
+		glm::mat4 modelaux(1.0);
+		glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 8.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+
+		pisoTexture.UseTexture();
+		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+
+		meshList[2]->RenderMesh();
+
+
+
+		//---------------Mundo Finn---------
+		//---------------Finn----------------
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f ,-1.0f, -3.0f + mainWindow.getmuevex()));
+		modelaux = model;
+		model = glm::translate(model, glm::vec3(0.0f, 3.688f, 0.0f));
+		//model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
+		//model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Finn.RenderModel();
+
+
+		////Mochila
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.058f, 3.94f, -0.832f));
+		/*model = glm::rotate(model, 90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));*/
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Mochila.RenderModel();
+
+		////B Izquierdo
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.747f, 4.092f, 0.0f));
+		/*model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));*/
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BIzq.RenderModel();
+
+		////B Derecha
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.84f, 4.085f, 0.0f));
+		/*model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));*/
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BDer.RenderModel();
+
+		////Pierna Izquierda
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.658f, 1.99f, 0.002f));
+		/*model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));*/
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		PIzq.RenderModel();
+
+		////Pierna Derecha
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.772f, 2.035f, 0.0f));
+		/*model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));*/
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		PDer.RenderModel();
+
+		
+		//Libro Enchiridion
+		model = glm::mat4(1.0);
+
+		//Ajuste del libro
+		model = glm::translate(model, glm::vec3(33.996f, -1.0f, 60.969f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Enchiridion.RenderModel();
+
+		//Golpear el Topo
+		model = glm::mat4(1.0);
+
+		//Ajuste del topo
+		model = glm::translate(model, glm::vec3(52.347f, -1.0f, 62.901f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Topo.RenderModel();
+
+		//Banca 
+		model = glm::mat4(1.0);
+
+		//Ajuste de la banca
+		model = glm::translate(model, glm::vec3(70.148f, -1.0f, 60.573f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Banca.RenderModel();
+
+	
+		//Puesto de pizzas 
+		model = glm::mat4(1.0);
+
+		//Ajuste del puesto
+		model = glm::translate(model, glm::vec3(66.018f, -1.0f, 47.256f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Pizza.RenderModel();
+		
+
+		//Mesa de picnic 
+		model = glm::mat4(1.0);
+
+		//Ajuste de la mesa
+		model = glm::translate(model, glm::vec3(69.657f, -1.0f, 27.955f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Picnic.RenderModel();
+
+
+		//Tiro al blanco
+		model = glm::mat4(1.0);
+
+		//Ajuste del juego
+		model = glm::translate(model, glm::vec3(52.694f, -1.0f, 23.692f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Tiro_blanco.RenderModel();
+
+
+		//Arco finn
+		model = glm::mat4(1.0);
+
+		//Ajuste del arco
+		model = glm::translate(model, glm::vec3(32.398f, -1.0f, 26.486f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Arco_finn.RenderModel();
+
+		//Letrero finn
+		model = glm::mat4(1.0);
+
+		//Ajuste del letrero
+		model = glm::translate(model, glm::vec3(32.426f, -1.0f, 26.541f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Letrero_finn.RenderModel();
+
+		//Prismo
+		model = glm::mat4(1.0);
+
+		//Ajuste de prismo
+		model = glm::translate(model, glm::vec3(33.996f, -1.0f, 47.936f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Prismo.RenderModel();
+
+
+
+
+
+		//-------------Mundo Invencible---------
+		//Nave vitrumita
+		model = glm::mat4(1.0);
+
+		//Ajuste de la nave
+		model = glm::translate(model, glm::vec3(34.8f, -1.0f, -20.219f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Nave.RenderModel();
+
+
+		//Puesto de regalos
+		model = glm::mat4(1.0);
+
+		//Ajuste de la nave
+		model = glm::translate(model, glm::vec3(54.313f, -1.0f, -17.491f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Regalos.RenderModel();
+
+
+		//Jaula de bateo
+		model = glm::mat4(1.0);
+
+		//Ajuste de la jaula
+		model = glm::translate(model, glm::vec3(66.804f, -1.0f, -37.29f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Bateo.RenderModel();
+		
+
+		//Juego de baloncesto
+		model = glm::mat4(1.0);
+
+		//Ajuste del juego
+		model = glm::translate(model, glm::vec3(54.313f, -1.0f, -60.732f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Baloncesto.RenderModel();
+
+		//Casa de eve
+		model = glm::mat4(1.0);
+
+		//Ajuste de la casa
+		model = glm::translate(model, glm::vec3(34.8f, -1.0f, -60.732f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Casa_arbol.RenderModel();
+
+		
+		//Mesa de picnic 
+		model = glm::mat4(1.0);
+
+		//Ajuste de la mesa
+		model = glm::translate(model, glm::vec3(47.498f, -1.0f, -40.246f));
+		//model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Picnic.RenderModel();
+
+
+
+
+		//blending: transparencia o traslucidez
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		//Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		//meshList[3]->RenderMesh();
+		glDisable(GL_BLEND);
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}
